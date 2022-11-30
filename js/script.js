103671ce13663579b486ca68ce3fb5386468c4b0
takaki-web <104483390+takaki-web@users.noreply.github.com>
2022-11-30 15:15:11 +0900

    脱jQuery・画像にwidth height追加

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
-// ページ内スクロール
-$('a[href^="#"]').click(function () {
-  const speed = 600;
-  let href = $(this).attr("href");
-  let target = $(href == "#" || href == "" ? "html" : href);
-  let position = target.offset().top;
-  $("body,html").animate({ scrollTop: position }, speed, "swing");
+var supportsPassive=false;try{var opts=Object.defineProperty({},'passive',{get:function(){supportsPassive=true;}});window.addEventListener("testPassive", null, opts);window.removeEventListener("testPassive", null, opts);} catch (e) {}
+// ページ内スクロール---------------------------------------------------
+const elem1 = document.querySelector('a[href^="#"]'); //単一の要素を取得
+const speed = 600; //スクロールスピード
+/*---スマホのときはtouchstart---*/
+let flag1 = false;
+elem1.addEventListener("touchstart",{},supportsPassive?{passive:true}:false, function () { //要素が押されたら
+  flag1 = true;
+  let href = elem1.getAttribute("href");/* 属性 */
+  let rect1 = href.getBoundingClientRect(); //ブラウザの表示領域の左上を基準とする相対座標を取得
+  let scrollTop = document.getElementById(href == "#" || href == "" ? "html" : href ); //スクロール量の取得
+  let position = rect1.top + scrollTop; //座標とスクロール量を合算
+  $("body,html").animate({ scrollTop: position }, speed, "swing"); //スピードも組み合わせて実行
   return false;
+});
+/*---PCではclick---*/
+elem1.addEventListener("click", function () { //要素がクリックされたら
+  if (flag1) {
+    flag1 = false;
+  } else {
+    let href = elem1.getAttribute("href");
+    let rect1 = href.getBoundingClientRect();
+    let scrollTop = document.getElementById(href == "#" || href == "" ? "html" : href );
+    let position = rect1.top + scrollTop;
+    $("body,html").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  }
 });
